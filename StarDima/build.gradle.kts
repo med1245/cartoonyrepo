6f7b34a96b8a4754a86ec4baae637db4043e735c
MehdiX <med1245@users.noreply.github.com>
2026-03-13 03:48:54 +0000

    Bump StarDima version to 2 and fix extractor robustness

--- StarDima/build.gradle.kts
+++ StarDima/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
 }
 
 // Use an integer for version numbers
-version = 1
+version = 2
 
 cloudstream {
     description = "Arabic cartoons, anime, series, and movies from stardima.com"
